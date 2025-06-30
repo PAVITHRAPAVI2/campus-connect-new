da67bf0de4048d82b078602b3cdbcca9f7ed9c13
Mugilan <lanmugi88@gmail.com>
2025-06-30 16:00:53 +0530

    final push

---
+++ campus-connect.Server/Model/ChatMessage.cs
@@ -0,0 +1,18 @@
+﻿      
+namespace campus_connect.Server.Model
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+
+        public required string SenderName { get; set; } // e.g., "John (Student)"
+        public required string Role { get; set; }       // "Student", "Faculty", "Admin"
+        public required string Department { get; set; } // e.g., "CS", "BCA"
+        public required string Message { get; set; }
+
+        public int Likes { get; set; } = 0;
+        public bool IsCommonChat { get; set; } = true;
+
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    }
+}
